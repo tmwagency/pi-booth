994d56e1789b7697747b2ee446e8f162f2377a81
Roo <awilliams@tmw.co.uk>
2014-03-06 14:24:13 +0000

    User table display on thread, sockets working and communicating with front end. TODO: change user table structure from dict of lists to two dimensional list and pass to JavaScript

---
+++ app/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+from flask.ext.socketio import SocketIO, emit
+from threading import Thread
+import user
+import time
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html", users = user.return_users())
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    emit('event', {'data': 'Connected'})
+    for i in range(0,100):
+        time.sleep(0.5)
+        emit('event',{'data': 'got it!!!', 'count': i})
+    time.sleep(50)
+    emit('event',{'data': 'still got it!!!'})
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected')
+
+if __name__ == '__main__':
+    socketio.run(app)
+
--- app/user.py
+++ app/user.py
@@ -2,22 +2,29 @@ import urllib2
 import sys
 from collections import defaultdict
 import config
+import Queue
+from threading import Thread
 
+q = Queue.Queue()
 
-
-def init_users(userfile_url):
+def init_users(q, userfile_url):
     users = defaultdict(list)
     data = urllib2.urlopen(userfile_url)
 
     for line in data.readlines():
         id, username, _ = line.split(", ", 2)
-        users[username[:1].lower()].append((username, id))
-    return users
+        users[username[:1].lower()].append((username, id))     
+    q.put(users)
+    print '---------------got users'
         
     
 
 def return_users():
-    users = init_users(config.user_file_url)
+    thr = Thread(target=init_users, args=(q, config.user_file_url,))
+    thr.start()
+    thr.join()
+    users = q.get()
+    # users = init_users(config.user_file_url)
     return users
 
 
