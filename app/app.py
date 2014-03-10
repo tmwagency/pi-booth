ff00cf3ecf5e41d27cf44a02645495ee215c967b
Roo <awilliams@tmw.co.uk>
2014-03-10 17:34:32 +0000

    Two steps back... Still can't get my head around threading to accept digital encoder input over GPIO and simaltaneously updating the GUI.

--- app/app.py
+++ app/app.py
@@ -3,7 +3,23 @@ from flask.ext.socketio import SocketIO, emit
 from threading import Thread
 import user
 import time
+import urllib2
+import sys
+from collections import defaultdict
+import config
+import Queue
+import gaugette.rotary_encoder
+import gaugette.switch
 
+A_PIN  = 7
+B_PIN  = 9
+SW_PIN = 8
+
+encoder = gaugette.rotary_encoder.RotaryEncoder.Worker(A_PIN, B_PIN)
+switch = gaugette.switch.Switch(SW_PIN)
+last_state = None
+
+q = Queue.Queue()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -13,7 +29,7 @@ socketio = SocketIO(app)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html", users = user.return_users())
+    return render_template("index.html", users = return_users())
 
 @socketio.on('connect', namespace='/test')
 def test_connect():
@@ -28,6 +44,25 @@ def test_connect():
 def test_disconnect():
     print('Client disconnected')
 
+def init_users(q, userfile_url):
+    users = defaultdict(list)
+    data = urllib2.urlopen(userfile_url)
+
+    for line in data.readlines():
+        id, username, _ = line.split(", ", 2)
+        users[username[:1].lower()].append((username, id))     
+    q.put(users)
+    print '---------------got users'
+        
+def return_users():
+    thr = Thread(target=init_users, args=(q, config.user_file_url,))
+    thr.start()
+    thr.join()
+    users = q.get()
+    # users = init_users(config.user_file_url)
+    return users
+
+print encoder.get_delta()
+
 if __name__ == '__main__':
     socketio.run(app)
-
